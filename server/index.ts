9b1f2adc4d1891179af48044c8f657dab701b2b3
whuang06 <william1993huang@gmail.com>
2025-04-28 01:20:31 +0000

    Publish project

--- server/index.ts
+++ server/index.ts
@@ -57,8 +57,6 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
   const port = 5000;
   server.listen({
     port,
